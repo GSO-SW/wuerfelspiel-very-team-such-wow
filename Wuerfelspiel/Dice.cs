a81c4f7310105a84b8f38e075074c7795e4581e7
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2022-03-31 09:08:06 +0200

    dice instantiate tests

---
+++ Wuerfelspiel/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Wuerfelspiel
+{
+    public class Dice
+    {
+        public Dice(int sides = 6)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Number
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        public int Sides
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        public bool Locked
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ WuerfelspielTests/DiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wuerfelspiel;
+
+namespace WuerfelspielTests
+{
+    [TestClass]
+    public class DiceTests
+    {
+        [TestMethod]
+        public void DiceInstantiateTest()
+        {
+            Dice dice = new Dice();
+        
+            Assert.IsNotNull(dice);
+            Assert.IsTrue(dice.Sides == 6);
+            Assert.IsTrue(dice.Locked == false);
+        } 
+        
+        [TestMethod]
+        public void DiceInstantiateWithSidesTest()
+        {
+            Dice dice = new Dice(12);
+            
+            Assert.IsNotNull(dice);
+            Assert.IsTrue(dice.Sides == 12);
+            Assert.IsTrue(dice.Locked == false);
+        }
+    }
+}
